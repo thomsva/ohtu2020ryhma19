58dc97b7f05205653034502850cd695e8c21cd91
Veera Lupunen <veera.lupunen@helsinki.fi>
2020-11-26 22:08:38 +0200

    Create basic text UI

--- ReadingTipLibrary/src/main/java/library/ui/ReadingTipUi.java
+++ ReadingTipLibrary/src/main/java/library/ui/ReadingTipUi.java
@@ -6,21 +6,74 @@
 package library.ui;
 
 import java.util.Scanner;
+import library.domain.ReadingTip;
 import library.domain.ReadingTipService;
 
 public class ReadingTipUi {
+    
+    Scanner scanner;
+    ReadingTipService service;
 
     public void start() throws Exception {
-        ReadingTipService ok = new ReadingTipService();
 
-        String type = "book";
-        String title = "The Hitchhiker's Guide to the Galaxy";
-        String info1 = "Douglas Adams";
-        String info2 = "	0-330-25864-8";
+        service = new ReadingTipService();
+        scanner = new Scanner(System.in);
 
-        ok.createTip(type, title, info1, info2);
-        ok.browseReadingTips();
+        System.out.println("Hello user!");
 
+        while (true) {
+            printOptions();
+            String input = scanner.nextLine();
+            if (input.equals("A")) {
+                createReadingTip();
+            } else if (input.equals("M")) {
+                System.out.println("This option is coming soon. Thank you for being patient!");
+            } else if (input.equals("L")) {
+                System.out.println("This option is coming soon. Thank you for being patient!");
+            } else if (input.equals("Q")) {
+                break;
+            } else {
+                System.out.println("Oops, command not existing! Try again.");
+            }
+        }
+    }
+
+    private void printOptions() {
+        System.out.println("You can...");
+        System.out.println("(A)dd a new reading tip"); //this is what we have at the moment
+        System.out.println("(M)odify an existing reading tip"); //coming soon
+        System.out.println("(L)ist all existing reading tips"); //coming soon; selecting this allows user to specify searching criteria
+        System.out.println("(Q)uit");
+    }
+    
+    private void createReadingTip() {
+        System.out.println("What is the title of the reading tip?");
+        String title = scanner.nextLine();
+        printTypes();
+        String type = scanner.nextLine();
+        ReadingTip tip = service.createTipWithType(type.toLowerCase(), title);
+        System.out.println(tip.toString());
+    }
+    
+    private void printTypes() {
+        System.out.println("What kind of reading tip it is?");
+        System.out.println("Options:");
+        System.out.println("Blogpost");
+        System.out.println("Book");
+        System.out.println("Podcast");
+        System.out.println("Video");
+    }
+
+//        ReadingTipService ok = new ReadingTipService();
+//
+//        String type = "book";
+//        String title = "The Hitchhiker's Guide to the Galaxy";
+//        String info1 = "Douglas Adams";
+//        String info2 = "	0-330-25864-8";
+//
+//        ok.createTip(type, title, info1, info2);
+//        ok.browseReadingTips();
+    
 //    public void start() throws Exception {
 //        ReadingTipService ok = new ReadingTipService();
 //        Scanner sc = new Scanner(System.in);
@@ -36,5 +89,4 @@ public class ReadingTipUi {
 //
 //
 //    }
-    }
 }
